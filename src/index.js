3e469544e006af2828de198046b2595ac7dd277e
Betsabé Mique <betsabe.mique.p@gmail.com>
2018-05-29 22:37:18 -0400

    modificación botones okey, arreglos html

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ function botonReset() {
     document.getElementById("text").value = "";
     document.getElementById("text1").value = "";
 }
+
+function cifra() {
+    let message = document.getElementById("text").value;
+    document.getElementById("msje1").innerHTML = message;
+}
+
+function decifra() {
+    let message = document.getElementById("text1").value;
+    document.getElementById("msje2").innerHTML = message;
+}
