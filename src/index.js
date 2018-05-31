92d3e44bce81f67a4eb0db056acaf5eb10c3d5ca
Betsabé Mique <betsabe.mique.p@gmail.com>
2018-05-30 20:15:18 -0400

    arreglos js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,28 @@ window.onload = () => {
 
     const btn1 = document.getElementById("btnCifrar");
 
-    encodeBtn.addEventListener("click", () => {
-        const strCifrar = document.getElementById("text").value;
-        const num1 = parseInt(document.getElementById("number").value);
+    btn1.addEventListener("click", () => {
+        const str =  document.getElementById("text").value;
+        const num1 = parseInt(document.getElementById("clave1").value);
         const msjeCifrar = window.cipher.encode(str, num1);
         document.getElementById("msje1").innerHTML = msjeCifrar;
-      });
+    });
 
-}
+    const btn2 = document.getElementById("btnDecifrar");
+
+    btn2.addEventListener("click", () => {
+        const str2 = document.getElementById("text1").value;
+        const num2 = parseInt(document.getElementById("clave2").value);
+        const msjeDecifrar =window.cipher.decode(str2, num2);
+        document.getElementById("msje2").innerHTML = msjeDecifrar;
+    });
+
+};
+
+
+     /*function botonReset() {
+        document.getElementById("clave1").value = "";
+        document.getElementById("text").value = "";
+        document.getElementById("msje1").value = "";
+    }*/
+
